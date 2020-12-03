67cc2c20ada5115cf017f987787dfd1db46a67de
Max Ivanov <maximus-dr@mail.ru>
2020-12-03 23:17:41 +0300

    Added changeLevel function

--- src/js/data/quest.js
+++ src/js/data/quest.js
@@ -1,8 +1,9 @@
-export const INITIAL_GAME = {
+export const INITIAL_GAME = Object.freeze({
   level: 0,
   lives: 2,
   time: 0
-};
+});
+
 
 export const quest = {
   'level-0': {
@@ -35,3 +36,15 @@ export const quest = {
     ]
   }
 };
+
+
+export function changeLevel(game, level) {
+  if (typeof level !== 'number') {
+    throw new Error('Level should be of type number');
+  }
+  if (level < 0) {
+    throw new Error('Level should not be negative value');
+  }
+  const newGame = Object.assign({}, game, {level});
+  return newGame;
+}
