ea2f62a5ae3e42078caa97da454d076324e3a152
Max Ivanov <maximus-dr@mail.ru>
2020-12-04 17:56:19 +0300

    Transported initial state from quest.js to data.js

--- src/js/data/quest.js
+++ src/js/data/quest.js
@@ -1,10 +1,3 @@
-export const INITIAL_GAME = Object.freeze({
-  level: 0,
-  lives: 2,
-  time: 0
-});
-
-
 export function changeLevel(game, level) {
   if (typeof level !== 'number') {
     throw new Error('Level should be of type number');
--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,25 @@
+import {INITIAL_STATE, levels} from './data/data';
+import headerTemplate from './header';
+
+
 function getElementFromTemplate(template) {
   const container = document.createElement('div');
   container.innerHTML = template;
   return container;
 }
 
-const screenTemplate = `
-  <header class="header">
-    <div>Мир: 0</div>
-    <div>
-      Жизни:
-      <span class="heart__empty">♡</span>
-      <span class="heart__full">♥</span>
-      <span class="heart__full">♥</span>
-    </div>
-    <div>Время: 0</div>
-  </header>
+const screenTemplate = (level) => `
   <div class="quest">
-    <p class="text">Вас зовут Луиджи Марио, вы водопроводчик, но сейчас перед вами стоит очень важная миссия — спасти принцессу Грибного Королевства Тоадстул Пич. Её похитил злой повелитель черепах Боузер и вы отправились в Грибное Королевство, чтобы победить Боузера и освободить принцессу. Вы отправляетесь в первый замок, но, чтобы в него попасть нужно преодолеть несколько препятствий. Вы стоите посреди на одной из равнин Грибного Королевства и видите как на вас стремительно несется хмурый гриб вашего роста. Нужно срочно что-то предпринять</p>
+    <p class="text">
+      ${level.text}
+    </p>
     <input type="text" id="quest__input">
     <ul class="answers">
-      <li class="answer">LEFT. Вы побежите влево, от гриба</li>
-      <li class="answer">RIGHT. Вы побежите вправо, прямо на гриб</li>
-      <li class="answer">JUMP. Вы прыгнете вверх</li>
+      ${
+        [...Object.entries(level.answers)].map(([answer]) => {
+          return `<li class="answer">${answer.toUpperCase()}</li>`;
+        }).join('')
+      }
     </ul>  
   </div>
   <div class="result"></div>
@@ -29,8 +27,10 @@ const screenTemplate = `
 `;
 
 const container = document.querySelector('#main');
-const screenElement = getElementFromTemplate(screenTemplate);
-container.append(screenElement);
+const screenElement = getElementFromTemplate(screenTemplate(levels[INITIAL_STATE.level]));
+const headerElement = getElementFromTemplate(headerTemplate);
+
+container.append(headerElement, screenElement);
 
 const input = document.querySelector('#quest__input');
 input.addEventListener('keydown', (e) => {
