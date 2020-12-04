caae4ca63823186cefd3324d27d28ace36d221cd
Max Ivanov <maximus-dr@mail.ru>
2020-12-04 17:58:21 +0300

    Added headerTemplate function

---
+++ src/js/header.js
@@ -0,0 +1,21 @@
+import {INITIAL_STATE} from './data/data';
+
+const headerTemplate = (state) => `
+  <header class="header">
+  <div>Мир: ${state.level}</div>
+  <div>
+    Жизни:
+    ${new Array(3 - state.lives)
+      .fill('<span class="heart__empty">♡</span>')
+      .join('')
+    }
+    ${new Array(state.lives)
+      .fill('<span class="heart__full">♥</span>')
+      .join('')
+    }
+  </div>
+  <div>Время: 0</div>
+  </header>
+`;
+
+export default headerTemplate(INITIAL_STATE);
