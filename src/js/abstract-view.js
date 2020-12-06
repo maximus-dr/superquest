084247b77e127a9ca87244cce01b8a7946c05efe
Max Ivanov <maximus-dr@mail.ru>
2020-12-06 17:26:29 +0300

    Added AbstractView class for creating view components

---
+++ src/js/abstract-view.js
@@ -0,0 +1,29 @@
+import {getElementFromTemplate} from "./util";
+
+export default class AbstractView {
+  constructor() {
+    if (new.target === AbstractView) {
+      throw new Error(`Can't instantiate AbstractView, only concrete one`);
+    }
+  }
+  get template() {
+    throw new Error('Template is required');
+  }
+
+  get element() {
+    if (this._element) {
+      return this._element;
+    }
+    this.element = this.render();
+    this.bind(this._element);
+    return this._element;
+  }
+
+  render() {
+    return getElementFromTemplate(this.template);
+  }
+
+  bind(element) {
+    // bind handlers if required
+  }
+}
