ecec3309ae2730605531ddd273b0cc960bd2fdb2
Max Ivanov <maximus-dr@mail.ru>
2020-12-09 23:44:23 +0300

    Added app.js as Presenter and moved all initialization logic from index.js to app.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,34 +1,4 @@
-import {INITIAL_STATE, levels} from './data/data';
-import Header from './components/header';
-import Level from './components/level';
+import { app } from "./app";
 
 
-const container = document.querySelector('#main');
-const header = new Header(INITIAL_STATE).element;
-
-container.before(header);
-
-function renderScreen(state) {
-  const current = state.level;
-  const level = levels[current];
-  const screen = new Level(level).element;
-  container.innerHTML = '';
-  container.append(screen);
-
-  const input = document.querySelector('#quest__input');
-  input.addEventListener('keydown', (e) => {
-    if (e.key === 'Enter') {
-      const userAnswer = input.value.trim().toLowerCase();
-      const destination = userAnswer in levels[state.level].answers
-        ? levels[state.level].answers[userAnswer]
-        : null;
-
-      if (destination) {
-        renderScreen(Object.assign({}, state, {'level': destination}));
-      }
-    }
-  });
-  input.focus();
-}
-
-renderScreen(INITIAL_STATE);
+app.init();
