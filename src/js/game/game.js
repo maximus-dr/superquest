25fde81ccc2d5024b17b7106d3344434260b7e44
Max Ivanov <maximus-dr@mail.ru>
2020-12-09 23:15:00 +0300

    Added game module with game logic

---
+++ src/js/game/game.js
@@ -0,0 +1,47 @@
+
+export class Game {
+
+  constructor(state) {
+    this._state = { ...state };
+    this.listeners = new Set();
+  }
+
+  get state() {
+    return this._state;
+  }
+
+  set state(newState) {
+    this._state = { ...newState };
+    this.broadcast();
+  }
+
+  subscribe(listener) {
+    this.listeners.add(listener);
+  }
+
+  unsubscribe(listener) {
+    this.listeners.delete(listener);
+  }
+
+  broadcast() {
+    for (const listener of this.listeners) {
+      listener();
+    }
+  }
+
+  update(param) {
+    this.state = Object.assign(this._state, param);
+  }
+
+  tick() {
+    this.update({time: this.state.time + 1});
+  }
+
+  startTimer() {
+    this._timerId = setInterval(() => this.tick(), 1000);
+  }
+
+  stopTimer() {
+    clearInterval(this._timerId)
+  }
+}
